12de881c45a5c219da0b011580fda86a540dc9d3
Shamn <gurshamnjotsingh@gmail.com>
2013-04-24 12:49:05 -0700

    Updated scripts and libraries

---
+++ scripts/functions.js
@@ -0,0 +1,8 @@
+// Function to load data from ChemDoodle servers.
+
+// ChemDoodle.iChemLabs.getMoleculeFromDatabase('morphine', {
+// 	'database' : 'pubchem'
+// }, function (mol) {
+// 	alertMolecular(mol);
+// });
+
